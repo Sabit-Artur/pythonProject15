5aea9ddda8c561a34fc0152e00574b853d1553ac
Sabit-Artur <krusl4372@gmail.com>
2023-02-20 03:14:23 +0300

    1

---
+++ main/utils.py
@@ -0,0 +1,33 @@
+import json
+from json import JSONDecodeError
+
+
+class PostHandler:
+    def __init__(self, path):
+        self.path = path
+
+    def load_posts(self):
+        posts = []
+        try:
+            with open(self.path, 'r', encoding='utf-8') as file:
+                posts = json.load(file)
+        except Exception as e:
+            print(e)
+
+        return posts
+
+    def search_posts(self, substr):
+        posts = []
+        for post in self.load_posts():
+            if substr.lower() in post['content'].lower():
+                posts.append(post)
+        return posts
+
+    def save_posts_to_json(self, posts):
+        with open(self.path, 'w', encoding='utf-8') as file:
+            json.dump(posts, file)
+
+    def add_post(self, post):
+        posts = self.load_posts()
+        posts.append(post)
+        self.save_posts_to_json(posts)
---
+++ main/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request
+import logging
+
+from main.utils import PostHandler
+
+main_blueprint = Blueprint('main_blueprint', '__names__', template_folder='templates')
+logging.basicConfig(filename='basic.log', level=logging.INFO)
+
+@main_blueprint.route('/')
+def main_page():
+    return render_template('index.html')
+
+
+@main_blueprint.route('/search')
+def search_page():
+    substr = request.args.get('s')
+    logging.info(f'Поиск:{substr}')
+    post_handler = PostHandler('posts.json')
+    posts = post_handler.search_posts(substr)
+    return render_template('post_list.html', posts=posts, substr=substr)
+
+
+
